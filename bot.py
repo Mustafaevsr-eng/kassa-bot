ce5e9ce3660e3ca2f960ffc5fd88696947dcd605
Mustafaevsr-eng <Mustafaev.sr@icloud.com>
2026-06-03 04:20:52 +0300

    Replace hardcoded token with environment variable

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import json
 import os
 from datetime import datetime
 
-TOKEN = os.getenv("8823603145:AAHe9yUAPRLoG0JicWIdsh5qQhUU04NaQXg")
+TOKEN = os.getenv("TOKEN")
 
 DATA_FILE = "kassa_ru.json"
 HISTORY_FILE = "history_ru.json"
